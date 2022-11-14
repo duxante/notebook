0ab5c4adf75c480b7c0f554015c93f73c54f901e
duxante <spomenko.dukic@gmail.com>
2022-11-14 23:14:21 +0100

    creating snackbars for delete and reactivate of users

--- src/Components/DeletedUsers/deletedUsers.component.jsx
+++ src/Components/DeletedUsers/deletedUsers.component.jsx
@@ -3,17 +3,37 @@ import Sidebar from "../../Common/Sidebar/sidebar.component";
 import Dashboard from "../../Common/DashboardFolder/dashboard.component";
 import { useState, useEffect } from "react";
 import fire from "../../Utils/firebase.config";
+import Notification from "../../Common/NotificationFolder/notification.component";
 
 
 const DeletedUsers = () => {
     const [deletedUsers, setDeletedUsers] = useState([]);
+    const [notificationConfig, setNotificationConfig] = useState({
+        visible: false,
+        severity: "",
+        text: "",
+    })
+  
 
     const fetchDeletedUsers = async () => {
         const db = fire.firestore();
         const response = db.collection("deletedUsers");
         const data = await response.get();
         const newDeletedUsers = data.docs.map((deletedUser) => {
-          return deletedUser.data();
+            const deletedUserData = deletedUser.data();
+            const singleDeletedUser = {
+                name: deletedUserData.name,
+                position: deletedUserData.position,
+                status: deletedUserData.status,
+                employed: deletedUserData.employed,
+                id: deletedUser.id,
+            }
+            setNotificationConfig({
+                visible: true,
+                severity: "success",
+                text: "User successfully reactivated!",
+            })
+            return singleDeletedUser;
         });
         setDeletedUsers([...newDeletedUsers]);
       };
@@ -23,15 +43,39 @@ const DeletedUsers = () => {
       }, []);
 
 
+    const handleReactivateUser = async (reactivatedUser)  => {
+        const db = fire.firestore();
+        db.collection("users/").add({
+            name: reactivatedUser.name,
+            position: reactivatedUser.position,
+            status: reactivatedUser.status,
+            employed: reactivatedUser.employed,
+            id: reactivatedUser.id,
+        });
+        await db.collection("deletedUsers").doc(reactivatedUser.id).delete();
+        fetchDeletedUsers();
+    }
+
+
+
     console.log(deletedUsers,'svi korisnici');
 
     return(
+        <>
+        {notificationConfig.visible &&
+        <Notification 
+            notificationConfig={notificationConfig}
+            setNotificationConfig={setNotificationConfig}
+        />
+        }
+
         <div className="holder-centralni-dio">
             <Sidebar />
             <Dashboard mainHeadingTitle="Deleted Users">
                 <div className="deletedUsersSubtitle">
                     <h4>PERSON</h4>
                     <h4>FUNCTION</h4>
+                    <h4>EMPLOYED</h4>
                     <h4>STATUS</h4>
                     <h4>ACTION</h4>
                 </div>
@@ -41,12 +85,14 @@ const DeletedUsers = () => {
                         <p key={Math.random()} className="deletedUserParagraph">{deletedUser.name}</p>
                         <p key={Math.random()} className="deletedUserParagraph">{deletedUser.position}</p>
                         <p key={Math.random()} className="deletedUserParagraph">{deletedUser.status}</p>
-                        <button key={Math.random()}>Reactivate</button>
+                        <p key={Math.random()} className="deletedUserParagraph">{deletedUser.employed}</p>
+                        <button key={Math.random()} onClick={() => handleReactivateUser(deletedUser)}>Reactivate</button>
                     </div>
                     )
                 })} 
             </Dashboard>
         </div>
+        </>
     )
 }
 
--- src/Components/Users/users.component.jsx
+++ src/Components/Users/users.component.jsx
@@ -3,18 +3,37 @@ import "./users.style.css";
 import Sidebar from "../../Common/Sidebar/sidebar.component";
 import Dashboard from "../../Common/DashboardFolder/dashboard.component";
 import fire from "../../Utils/firebase.config";
+import Notification from "../../Common/NotificationFolder/notification.component";
 
 
 
 const Users = () => {
     const [users, setUsers] = useState([]);
+    const [notificationConfig, setNotificationConfig] = useState({
+      visible: false,
+      severity: "",
+      text: "",
+  })
 
     const fetchUsers = async() => {
         const db = fire.firestore();
         const response = db.collection("users");
         const data = await response.get();
         const newUsers = data.docs.map((user) => {
-          return user.data();
+          const userData = user.data();
+          const singleUserData = {
+            name: userData.name,
+            position: userData.position,
+            employed: userData.employed,
+            status: userData.status,
+            id: user.id,
+          }
+          setNotificationConfig({
+            visible: true,
+            severity: "info",
+            text: "User sucssesfully deleted!",
+        })
+          return singleUserData;
         });
         setUsers([...newUsers]);
       };
@@ -22,33 +41,56 @@ const Users = () => {
     useEffect(() => {
         fetchUsers()
       }, []);
+
+    
+    const handleDeleteUser = async (user) => {
+      const db = fire.firestore();
+      db.collection("deletedUsers/").add({
+        name: user.name,
+        position: user.position,
+        employed: user.employed,
+        status: user.status,
+      });
+      await db.collection("users").doc(user.id).delete();
+      fetchUsers();
+    }
+
     
     console.log(users,'svi korisnici');
       
     return(
-        <div className="holder-centralni-dio">
-            <Sidebar />
-            <Dashboard mainHeadingTitle="Users">
-               <div className="usersSubtitles">
-                    <h4>NAME</h4>
-                    <h4>POSITION</h4>
-                    <h4>EMPLOYED</h4>
-                    <h4>STATUS</h4>
-                    <h4>ACTION</h4>
-                </div>
-                {users.map((user, index) => {
-                  return(
-                    <div key={index} className="user">
-                    <p key={Math.random()} className="userParagraph">{user.name}</p>
-                    <p key={Math.random()} className="userParagraph">{user.position}</p>
-                    <p key={Math.random()} className="userParagraph">{user.employed}</p>
-                    <p key={Math.random()} className="userParagraph">{user.status}</p>
-                    <button key={Math.random()}>Delete</button>
-                    </div>
-                  )
-                })}
-            </Dashboard>
-        </div>
+      <>
+      {notificationConfig.visible && 
+      <Notification 
+          notificationConfig={notificationConfig}
+          setNotificationConfig={setNotificationConfig}
+      />
+      }
+      
+      <div className="holder-centralni-dio">
+          <Sidebar />
+          <Dashboard mainHeadingTitle="Users">
+              <div className="usersSubtitles">
+                  <h4>NAME</h4>
+                  <h4>POSITION</h4>
+                  <h4>EMPLOYED</h4>
+                  <h4>STATUS</h4>
+                  <h4>ACTION</h4>
+              </div>
+              {users.map((user, index) => {
+                return(
+                  <div key={index} className="user">
+                  <p key={Math.random()} className="userParagraph">{user.name}</p>
+                  <p key={Math.random()} className="userParagraph">{user.position}</p>
+                  <p key={Math.random()} className="userParagraph">{user.employed}</p>
+                  <p key={Math.random()} className="userParagraph">{user.status}</p>
+                  <button key={Math.random()} onClick={() => handleDeleteUser(user)}>Delete</button>
+                  </div>
+                )
+              })}
+          </Dashboard>
+      </div>  
+    </>
     )
 }
 
