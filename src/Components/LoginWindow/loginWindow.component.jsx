5ff043a1c42dff4e982282c4499787695b311bf7
duxante <spomenko.dukic@gmail.com>
2022-12-22 19:20:25 +0100

    refactoring LogIn component

--- src/Components/LoginWindow/loginWindow.component.jsx
+++ src/Components/LoginWindow/loginWindow.component.jsx
@@ -1,64 +1,76 @@
 import "./loginWindow.style.css";
-import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import MainHeading from "../../Common/MainHeading/mainHeading.component";
 import Button from "../../Common/ButtonFolder/button.component";
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import { TextField } from "@mui/material";
 
+const initialLoginValues = {
+    email:"",
+    password:""
+};
+
+const loginUserScheme = yup.object().shape({
+    email: yup.string().email("Email is invalid").required("Email is required"),
+    password: yup.string().required("Password is required"),
+});
 
 const LoginWindow = () => {
-    const [isFormVisible, setIsFormVisible] = useState(true);
-    const [isEmailValid, setIsEmailValid] = useState(true);
-    const [isPasswordValid, setIsPasswordValid] = useState(true);
-    const [userData, setUserData] = useState({
-        email:``,
-        password:``
-    })
-
-    const handleChangeUserData = (e) => {
-        e.preventDefault();
-        setUserData({
-            ...userData,
-            [e.target.name]: e.target.value
-        })
-    }
-
     const navigate = useNavigate();
-
-    const handleSubmitUserData = (e) => {
-        e.preventDefault();
-        if (userData.email === 'admin@admin.com' && userData.password === 'admin'){
-            setIsEmailValid(true);
-            setIsFormVisible(false);
+    const handleLoginSubmit = (values, onSubmitProps) => {
+        if (values.email === 'admin@admin.com' && values.password === 'admin'){
+            onSubmitProps.resetForm();
             navigate('/users');
-        } else {
-            setIsEmailValid(false);
-            setIsPasswordValid(false);
-            setIsFormVisible(true);
-        }
+        } 
     }
 
     return(
         <div className="mainLoginHolder">
-            <div className={isFormVisible ? "loginHolder" : "none"}>
-
-                <MainHeading 
-                mainHeadingTitle="Log in"
-                />
-                
-                <div className="addUserForm">
-                    <label>
-                        <input placeholder="Email" onChange={handleChangeUserData} name="email"/>   
-                    </label>
-                    {isEmailValid ? null : <span className="spanWarning">Email je neispravan, pokušajte ponovo.</span>}
-                    <label>
-                        <input placeholder="Password" onChange={handleChangeUserData}name="password"/>
-                    </label>
-                    {isPasswordValid ? null : <span className="spanWarning">Password je neispravan, pokušajte ponovo.</span>}
+            <div className="loginHolder">
+                <MainHeading mainHeadingTitle="Log in" />
+                <div className="formHolder">
+                    <Formik
+                        onSubmit={handleLoginSubmit}
+                        initialValues={initialLoginValues}
+                        validationSchema={loginUserScheme}
+                    >
+                        {({
+                            values,
+                            errors,
+                            touched,
+                            handleBlur,
+                            handleChange,
+                            handleSubmit
+                        }) => (
+                            <form className="addUserForm" onSubmit={handleSubmit}>
+                                <TextField
+                                    label="Email"
+                                    name="email"
+                                    onBlur={handleBlur}
+                                    onChange={handleChange}
+                                    value={values.email}
+                                    error={Boolean(touched.email) && Boolean(errors.email)}
+                                    helperText={touched.email && errors.email}
+                                />
+                                <TextField
+                                    label="Password"
+                                    name="password"
+                                    type="password"
+                                    onBlur={handleBlur}
+                                    onChange={handleChange}
+                                    value={values.password}
+                                    error={Boolean(touched.password) && Boolean(errors.password)}
+                                    helperText={touched.password && errors.password}
+                                />
+                                <Button buttonText="LOGIN" type="submit" />
+                                <p className="orSignUp">Or <Link to="/signUp"> Sign Up</Link></p>
+                                <p className="enjoyLife">enjoy life...</p>
+                                <p className="created">&copy; created by <b>Dux</b></p>      
+                            </form>
+                        )}
+                    </Formik>
                 </div>
-                    <Button buttonText="Submit" onClick={handleSubmitUserData}/>
-                    <p className="orSignUp">Or <Link to="/signUp"> Sign Up</Link></p>
-                    <p className="enjoyLife">enjoy life...</p>
-                    <p className="created">&copy; created by <b>Dux</b></p>  
             </div>
         </div>
         
